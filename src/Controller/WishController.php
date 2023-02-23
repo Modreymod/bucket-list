cba7490e18f3c9cb240064c82ec4e689cedbe7ef
agillet2022 <audreymaud.gillet2022@campus-eni.fr>
2023-02-23 14:47:32 +0100

    tp inserer liste et affichzer details

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,17 +10,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class WishController extends AbstractController
 {
     #[Route('/list', name: 'list')]
-    public function list(): Response
+    public function list(WishRepository $wishRepository): Response
     {
         //TODO récupérer la liste des wish et la renvoyer
-        return $this->render("wish/list.html.twig");
+        $wishes = $wishRepository->findBy(["isPublished"=>true],["dateCreated"=>"DESC"]);
+        return $this->render('wish/list.html.twig', ['wishes' => $wishes]);
     }
 
     #[Route('/{id}', name: 'show',requirements: ['id'=> '\d+'])]
-    public function show(int $id): Response
+    public function show(int $id,WishRepository $wishRepository ): Response
     {
         //TODO récupérer le wish et le renvoyer
-        return $this->render("wish/show.html.twig");
+        $wish = $wishRepository->find($id);
+        if(!$wish){
+            throw $this->createNotFoundException("Oops ! Wish not found !");
+        }
+        return $this->render("wish/show.html.twig", ['wish' => $wish]);
     }
 
 }
